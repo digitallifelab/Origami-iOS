751126140b2827c48f27ba76de9c9c6ba52afd0b
Ivan Yavorin <just1member@gmail.com>
2015-12-04 14:37:39 +0200

    fixed <"elements for visualization"> output.

--- Origami/LocalDatabaseHandler.swift
+++ Origami/LocalDatabaseHandler.swift
@@ -855,7 +855,7 @@ class LocalDatabaseHandler
         
         let fetchVisualizableElements = NSFetchRequest(entityName: "DBElement")
         
-        fetchVisualizableElements.predicate = NSPredicate(format: "dateCreated = nil")
+        fetchVisualizableElements.predicate = NSPredicate(format: "dateArchived = nil")
         
         var foundElements:[DBElement]?
         context.performBlockAndWait() {
