b2851dd9ab7b4d4093b58bd51c8ce502918c74ce
Oleg Kovalyok <Oleg-MacBook@macbook-pro.local>
2016-01-28 18:44:29 +0200

    add sort elements

--- Origami/Visualisation/VisualizationViewController.m
+++ Origami/Visualisation/VisualizationViewController.m
@@ -30,6 +30,7 @@
 @property (nonatomic, assign) CGPoint               startCenter;
 @property (nonatomic, strong) UIView                *contentView;
 @property (nonatomic, strong) OKVisualizationLayer  *elementView;
+@property (nonatomic, strong) NSMutableArray        *elementsArray;
 
 @end
 
@@ -52,7 +53,7 @@
     if (!self.objectsToVisualize)
     {
         NSArray * array = [[DataSource sharedInstance] getVisualizableContent];
-        self.objectsToVisualize =  [NSMutableArray arrayWithArray:array];
+        self.objectsToVisualize = [NSMutableArray arrayWithArray:array];
     }
     
     [self prepareElementsViewAndShow];
@@ -68,10 +69,8 @@
             [view removeFromSuperview];
         }
         
-        // TODO: 02.2 - сделал корректное распределение объектов в массивы: родитель -  подчиненные элементы
         NSArray *sortedObjects = [self getSortedObjects];
         
-        // TODO: 03.3 доработал расчет размера скроллвью: исправил ошибки
         CGFloat indentX = 175.f;
         CGFloat indentY = 60.f;
         
@@ -86,9 +85,8 @@
         
         _scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
         _scrollView.backgroundColor = [UIColor colorWithRed: 33.0/255.0 green: 150.0/255.0 blue: 243.0/255.0 alpha: 1.0];
-        //kDayCellBackgroundColor;//[UIColor colorWithRed:0.f green:0.5 blue:1.f alpha:1.f];//[UIColor colorWithWhite:0.9 alpha:1.f];
         _scrollView.contentSize = scrollViewSize;
-        _scrollView.contentInset = UIEdgeInsetsMake(64, 0, 44, 0);
+        _scrollView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
         _scrollView.minimumZoomScale = 1;
         _scrollView.maximumZoomScale = 3;
         _scrollView.delegate = self;
@@ -104,13 +102,25 @@
         
         CGPoint aPoint = CGPointMake(75.f, scrollViewHeight - 50.f);
         _elementView = [[OKVisualizationLayer alloc] init];
+        
+        BOOL firstElement = YES;
+        
         for (NSArray *rowArray in sortedObjects) {
             
             for (VisualizableObject *obj in rowArray) {
                 
+                if (firstElement & (obj.rootElementId == 0)) {
+                    firstElement = NO;
+                    aPoint.y = scrollViewHeight - 50.f;
+                } else if (obj.rootElementId == 0) {
+                    aPoint.y = scrollViewHeight - 50.f;
+                    aPoint.x = aPoint.x + indentX;
+                }
+                
                 OKVisualizationLayer *view = [_elementView getElementView:obj at:CGPointMake(aPoint.x, aPoint.y)];
-                view.elementId = obj.elementId;
-                view.rootElementId = obj.elementId;
+                view.elementId      = obj.elementId;
+                view.rootElementId  = obj.rootElementId;
+                
                 // Add the drag gesture recognizer with default values.
                 BFDragGestureRecognizer *holdDragRecognizer = [[BFDragGestureRecognizer alloc] init];
                 [holdDragRecognizer addTarget:self action:@selector(dragRecognized:)];
@@ -119,19 +129,11 @@
                 aPoint.y = aPoint.y - indentY;
                 
                 [_contentView addSubview:view];
-            }
-            
-            // FIX:
-            if ([rowArray isEqual:[sortedObjects lastObject]]) {
-                aPoint.y = scrollViewHeight - 50.f;
-                //aPoint.x = aPoint.x + indentX;
-            } else {
-                aPoint.y = scrollViewHeight - 50.f;
-                aPoint.x = aPoint.x + indentX;
+                
             }
             
         }
-
+        
     }
 }
 
@@ -140,65 +142,71 @@
 - (NSArray*)getSortedObjects
 {
     
-    NSArray *sortedObjects = [self.objectsToVisualize sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+    NSArray *elementsHierarchy = [self getElementsSortHierarchy:self.objectsToVisualize];
+    
+    NSArray *sortedElements    = [self getSortedElementsInArrays:elementsHierarchy];
+    
+    return sortedElements;
+    
+}
+
+- (NSArray *)getElementsSortHierarchy:(NSArray *)Data
+{
+    
+    NSMutableDictionary *element = [NSMutableDictionary dictionary];
+    [element setObject:[NSNumber numberWithInt:0] forKey:@"elementId"];
+    
+    self.elementsArray = [[NSMutableArray alloc] init];
+    
+    [self recursiveFunction:Data parentElement:element];
+    
+    return self.elementsArray;
+
+}
+
+- (void)recursiveFunction:(NSArray *)array parentElement:(id)parentElement
+{
+    
+    NSArray *subElements = [self findSubElementsForElementID:[[parentElement valueForKey:@"elementId"] intValue] elementData:array];
+    
+    for (VisualizableObject *object in subElements) {
         
-        NSInteger firstRootId  = [(VisualizableObject*)obj1 rootElementId];
-        NSInteger secondRootId = [(VisualizableObject*)obj2 rootElementId];
+        [self.elementsArray addObject:object];
+
+        [self recursiveFunction:array parentElement:object];
         
-        NSInteger firstElementId  = [(VisualizableObject*)obj1 elementId];
-        NSInteger secondElementId = [(VisualizableObject*)obj2 elementId];
-        
-        if ( firstRootId < secondRootId & firstElementId < secondElementId ) {
-            return (NSComparisonResult)NSOrderedAscending;
-        } else if ( firstRootId > secondRootId & firstElementId > secondElementId ) {
-            return (NSComparisonResult)NSOrderedDescending;
-        } else {
-            return (NSComparisonResult)NSOrderedSame;
-        }
-        
+    }
+    
+}
+
+- (NSArray *)findSubElementsForElementID:(int)elementID elementData:(NSArray *)elementData
+{
+    
+    NSIndexSet *indexsForFilteredElements = [elementData indexesOfObjectsPassingTest:^(id obj, NSUInteger idx, BOOL *stop) {
+        return (BOOL)([[obj valueForKey:@"rootElementId"] intValue] == elementID);
     }];
     
+    return [elementData objectsAtIndexes:indexsForFilteredElements];
     
-    CGFloat countRootElements = 0;
-    
-    for (VisualizableObject *obj in sortedObjects) {
-        //NSLog(@"index = %d, rootElementId = %d, elementID = %d", [sortedObjects indexOfObject:obj], obj.rootElementId, obj.elementId);
-        if (obj.rootElementId == 0) {
-            countRootElements++;
-        }
-    }
-    
-    CGFloat countElementsWithoutRootElements = sortedObjects.count - countRootElements;
-    
+}
 
-    NSMutableArray *rootElementsArray = [[NSMutableArray alloc] init];
-    
-    for (int i = 0; i < countRootElements; i++) {
-        
-        NSMutableArray *subArray = [[NSMutableArray alloc] init];
-        
-        for (int j = countElementsWithoutRootElements; j > 0; j--) {
-            
-            // add a root element
-            if (j == countElementsWithoutRootElements) {
-                [subArray addObject:[sortedObjects objectAtIndex:i]];
-            }
-            
-            // add elements
-            [subArray addObject:[sortedObjects objectAtIndex:j]];
-            
-        }
-        
-        [rootElementsArray addObject:subArray];
-        
-    }
-    
-    return rootElementsArray;
-    
+- (NSDictionary *)indexKeyedDictionaryFromArray:(NSArray *)array
+{
+    VisualizableObject *objectInstance;
+    NSUInteger indexKey = 0U;
+
+    NSMutableDictionary *mutableDictionary = [[NSMutableDictionary alloc] init];
+    for (objectInstance in array)
+
+        [mutableDictionary setObject:objectInstance forKey:[NSNumber numberWithUnsignedInt:objectInstance.elementId]];
+    indexKey++;
+
+    return (NSDictionary *)mutableDictionary;
 }
 
 - (CGFloat)getMaxCountElementsInRow:(NSArray *)sortedObjects
 {
+    
     CGFloat result = 0;
     for (NSArray *obj in sortedObjects) {
         if (obj.count > result) {
@@ -209,11 +217,77 @@
     return result;
 }
 
-/////////// kjhfbdjlzxhdbf jdslbf dsaff
+- (NSArray *)getSortedElementsInArrays:(NSArray *)elementsHierarchy
+{
+    
+    // 1. create arrays: rootElement + subElements
+    NSMutableArray *subRowsArray = [[NSMutableArray alloc] init];
+    NSMutableArray *subElements  = [[NSMutableArray alloc] init];
+    
+    for (VisualizableObject *obj in elementsHierarchy) {
+        
+        if (obj.rootElementId == 0) {
+            
+            if ([subElements count] > 0) {
+                [subRowsArray addObject:subElements];
+                subElements = [[NSMutableArray alloc] initWithObjects:obj, nil];
+            } else {
+                [subElements addObject:obj];
+            }
+            
+        } else if ([obj isEqual:[elementsHierarchy lastObject]]) {
+            [subElements addObject:obj];
+            [subRowsArray addObject:subElements];
+        } else {
+            [subElements addObject:obj];
+        }
+        
+    }
+    
+    // 2. sort arrays: rootElement is first element in a row + subElements, which sort by changeDate
+    NSArray *nonProcessedElements = [subRowsArray sortedArrayUsingComparator: ^(id obj1, id obj2) {
+        if ([obj1 count] > [obj2 count]) {
+            return (NSComparisonResult)NSOrderedDescending;
+        }
+        
+        if ([obj1 count] < [obj2 count]) {
+            return (NSComparisonResult)NSOrderedAscending;
+        }
+        return (NSComparisonResult)NSOrderedSame;
+    }];
+    
+    NSMutableArray *processedElements = [NSMutableArray array];
+    
+    for (NSArray *notSortedArray in nonProcessedElements) {
+        
+        NSArray *sortedArray = [notSortedArray sortedArrayUsingComparator:^NSComparisonResult(VisualizableObject *obj1, VisualizableObject *obj2) {
+            
+            if (obj1.rootElementId > 0 && obj2.rootElementId > 0)  {
+                
+                if ([obj1.changeDate compare:obj2.changeDate] == NSOrderedDescending) {
+                    return (NSComparisonResult)NSOrderedAscending;
+                } else if ([obj1.changeDate compare:obj2.changeDate] == NSOrderedAscending) {
+                    return (NSComparisonResult)NSOrderedDescending;
+                }
+                return (NSComparisonResult)NSOrderedSame;
+            }
+            
+            return (NSComparisonResult)NSOrderedSame;
+        }];
+        
+        [processedElements addObject:sortedArray];
+        
+    }
+    
+    return processedElements;
+    
+}
+
 
 #pragma mark - BFDragGestureRecognizer
 
-- (void)dragRecognized:(BFDragGestureRecognizer *)recognizer {
+- (void)dragRecognized:(BFDragGestureRecognizer *)recognizer
+{
     
     OKVisualizationLayer *view = (OKVisualizationLayer*)recognizer.view;
     
@@ -224,7 +298,6 @@
         if ([subviews count] == 0) return;
         
         UILabel *label = subviews[0];
-        //UILabel *serviceLabel = subviews[1];
         
         UIColor *borderColor;
         if ([view.backgroundColor isEqual:[UIColor colorWithRed:1.f green:0.f blue:0.5 alpha:1.f]]) {
@@ -268,14 +341,14 @@
     }
 }
 
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView1 {
-    // TODO: if zoom == maxSize then titleColour = white
-    // else titleColour = clear
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView1
+{
     return _contentView;
 }
 
 
 #pragma mark - LGAlertViewDelegate
+
 - (void)alertView:(LGAlertView *)alertView buttonPressedWithTitle:(NSString *)title index:(NSUInteger)index
 {
     
