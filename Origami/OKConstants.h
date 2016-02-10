65b6f22974de303ac547d11a795e40f0b7572532
Oleg Kovalyok <Oleg-MacBook@macbook-pro.local>
2016-02-10 19:59:44 +0200

    add Constants file

---
+++ Origami/OKConstants.h
@@ -0,0 +1,13 @@
+//
+//  OKConstants.h
+//  Origami
+//
+//  Created by Oleg Kovalyok on 22.01.16, Fr.
+//  Copyright © 2016 CloudCraft. All rights reserved.
+//
+
+#ifndef OKConstants_h
+#define OKConstants_h
+#define BlueColor [UIColor colorWithRed: green: blue: alpha:]
+
+#endif /* OKConstants_h */
